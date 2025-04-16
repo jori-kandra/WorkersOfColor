f51c639236ea5bf888c7baf28d614c70854cf524
jori-kandra <jkandra@epi.org>
2025-04-16 17:40:16 -0400

    clean up aggregation

--- WOC_annual_epop.R
+++ WOC_annual_epop.R
@@ -25,20 +25,21 @@ basic_clean <- basic_raw %>%
 
 cps_annual_epop <- basic_clean %>% 
   filter(bhaa2 != "other") %>% 
-  group_by(year, bhaa2) %>% 
-  summarize(epop = round(weighted.mean(emp, w = basicwgt/12, na.rm = TRUE), 3),
-            n = n()) %>% ungroup() %>% 
-  pivot_wider(id_cols = year, names_from = bhaa2, values_from = c("epop", "n")) 
-  
+  summarize(epop = round(weighted.mean(emp, 
+                                       # adjust weight for annual estimates
+                                       w = basicwgt/12, na.rm = TRUE), 3),
+            # create weighted sums
+            n = sum(emp * (basicwgt/12), na.rm = TRUE),
+            # .by keeps grouping for just this operation 
+            .by = c(year, bhaa2)) %>% 
+  pivot_wider(id_cols = year, names_from = bhaa2, values_from = c("epop", "n"))
+
 cps_annual_paepop <- basic_clean %>% 
   filter(bhaa2 != "other", age >= 25 & age <= 54) %>%
-  group_by(date, bhaa2) %>%   
-  summarize(pa_epop = round(weighted.mean(emp, w = basicwgt, na.rm = TRUE), 3),
-            n = n()) %>% ungroup() %>% 
-  pivot_wider(id_cols = date, names_from = bhaa2, values_from = c("pa_epop", "n")) %>% 
-  mutate(year = format(as.Date(date, format="%d/%m/%Y"),"%Y")) %>% 
-  group_by(year) %>% 
-  summarize(across(contains("pa_epop") | contains("n_"), ~round(mean(.x), 3)))
+  summarize(pa_epop = round(weighted.mean(emp, w = basicwgt/12, na.rm = TRUE), 3),
+            n = n(),
+            .by = c(year, bhaa2)) %>%
+  pivot_wider(id_cols = date, names_from = bhaa2, values_from = c("pa_epop", "n"))
   
 civilian_lf_benchmark <- basic_clean %>% 
   filter(lfstat == 1 | lfstat == 2) %>% 
@@ -47,13 +48,10 @@ civilian_lf_benchmark <- basic_clean %>%
                           (raceorig == 4 | raceorig == 5) & hispanic == 0 ~ "asian_nh",
                            raceorig == 3 ~ "AIAN_only",
                           TRUE ~ NA)) %>% 
-  group_by(date, wbhaa) %>% 
-  summarize(laborforce = sum(basicwgt, na.rm = TRUE)) %>% ungroup() %>% 
-  pivot_wider(id_cols = date, names_from = wbhaa, values_from = laborforce) %>% 
-  mutate(year = format(as.Date(date, format="%d/%m/%Y"),"%Y")) %>% 
-  group_by(year) %>% 
-  summarize(across(everything(), ~mean(.x))) %>% 
-  select(-date)
+  arrange(year, wbhaa) %>% 
+  summarize(laborforce = sum(basicwgt/12, na.rm = TRUE),
+            .by = c(year, wbhaa)) %>% 
+  pivot_wider(id_cols = year, names_from = wbhaa, values_from = laborforce)
 
 
 cps_annual_laborforce <- basic_clean %>% 
@@ -64,21 +62,14 @@ cps_annual_laborforce <- basic_clean %>%
                            (raceorig == 4 | raceorig == 5 | raceorig == 6) & hispanic == 0 ~ "aapi_alone_nh",
                            hispanic == 1 ~ "hispanic",
                            TRUE ~ "other")) %>% 
-  group_by(date, wbhaa) %>% 
-  summarize(laborforce = sum(basicwgt, na.rm = TRUE)) %>% ungroup() %>% 
-  pivot_wider(id_cols = date, names_from = wbhaa, values_from = laborforce) %>% 
-  mutate(year = format(as.Date(date, format="%d/%m/%Y"),"%Y")) %>% 
-  group_by(year) %>% 
-  summarize(across(everything(), ~mean(.x))) %>% 
-  select(-date)
+  arrange(year, wbhaa) %>% 
+  summarize(laborforce = sum(basicwgt/12, na.rm = TRUE),
+            .by = c(year, wbhaa)) %>%  
+  pivot_wider(id_cols = year, names_from = wbhaa, values_from = laborforce) 
   
 cps_annual_laborforce_all <- basic_clean %>% 
   filter(lfstat == 1 | lfstat == 2) %>% 
-  group_by(date) %>% 
-  summarize(laborforce = sum(basicwgt, na.rm = TRUE)) %>% ungroup() %>% 
-  mutate(year = format(as.Date(date, format="%d/%m/%Y"),"%Y")) %>% 
-  group_by(year) %>% 
-  summarize(laborforce_all = mean(laborforce)) %>% 
+  summarize(laborforce_all = sum(basicwgt/12, na.rm = TRUE), .by = year) %>%  
   left_join(cps_annual_laborforce, by = c("year")) %>% 
   select(-other) %>% 
   mutate(across(contains("alone") | contains("hispanic"), ~.x / laborforce_all))
